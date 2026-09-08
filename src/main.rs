5a1b3bfa256241a0c07fdd82b50f6331fd0c5395
Robert Serrano Kobylyansky <robertkobylyansky@gmail.com>
2026-09-08 23:30:23 +0200

    port to wayland

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 #![forbid(unsafe_code)]
 
+use std::sync::Arc;
 use log::error;
-use pixels::{Error, Pixels, SurfaceTexture};
+use pixels::{Pixels, SurfaceTexture};
 use winit::dpi::LogicalSize;
-use winit::event::{Event, VirtualKeyCode};
-use winit::event_loop::{ControlFlow, EventLoop};
-use winit::window::WindowBuilder;
+use winit::event::{DeviceEvent, DeviceId, Event, StartCause, WindowEvent};
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
 use winit_input_helper::WinitInputHelper;
 use num_complex::Complex64;
 use rayon::prelude::*;
+use winit::application::ApplicationHandler;
+use winit::keyboard::KeyCode;
+use winit::window::{Window, WindowAttributes, WindowId};
 
 const RESOLUTION: u32 = 800;
 
@@ -85,59 +88,78 @@ impl MandelbrotSet {
     }
 }
 
-fn main() -> Result<(), Error> {
-    env_logger::init();
-    let event_loop = EventLoop::new();
-    let mut input = WinitInputHelper::new();
-    let window = {
+struct InitializedApp {
+    resize_count: i32,
+    window: Arc<Window>,
+    pixels: Pixels<'static>,
+    mandelbrot: MandelbrotSet,
+    input: WinitInputHelper,
+}
+
+struct App(Option<InitializedApp>);
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         let size = LogicalSize::new(RESOLUTION as f64, RESOLUTION as f64);
-        WindowBuilder::new()
-            .with_title("Mandelbrot")
+        let window = Arc::new(event_loop.create_window(WindowAttributes::default().with_title("Mandelbrot")
             .with_inner_size(size)
-            .with_min_inner_size(size)
-            .build(&event_loop)
-            .unwrap()
-    };
+            .with_min_inner_size(size)).unwrap());
 
-    let mut pixels = {
-        let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(RESOLUTION as u32, RESOLUTION as u32, surface_texture)?
-    };
+        let mut mandelbrot = MandelbrotSet::new();
+        mandelbrot.calculate();
+        mandelbrot.color_function = |c| {
+            hsv_to_rgb([c, 255, 255])
+        };
 
-    let mut mandelbrot = MandelbrotSet::new();
-    mandelbrot.calculate();
-    mandelbrot.color_function = |c| {
-        hsv_to_rgb([c, 255, 255])
-    };
-    
-    let mut resize_count = 0;
-    event_loop.run(move |event, _, control_flow| {
+        let pixels: Pixels<'static> = {
+            let window_size = window.inner_size();
+            let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, window.clone());
+            Pixels::new(RESOLUTION as u32, RESOLUTION as u32, surface_texture).unwrap()
+        };
+
+        let input = WinitInputHelper::new();
+
+        std::mem::swap(self, &mut App(Some(InitializedApp {
+            resize_count: 0,
+            window,
+            pixels,
+            mandelbrot,
+            input,
+        })))
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, window_id: WindowId, event: WindowEvent) {
+        let app = self.0.as_mut().unwrap();
+
         // Draw the current frame
-        if let Event::RedrawRequested(_) = event {
-            if resize_count > 0 {
-                let size = window.inner_size();
-                pixels.resize(size.width, size.height);
-                resize_count -= 1;
+        if let WindowEvent::RedrawRequested = event {
+            if app.resize_count > 0 {
+                let size = app.window.inner_size();
+                app.pixels.resize_surface(size.width, size.height).unwrap();
+                app.resize_count -= 1;
             }
 
-            mandelbrot.draw(pixels.get_frame());
-            
-            if pixels
+            app.mandelbrot.draw(app.pixels.frame_mut());
+
+            if app.pixels
                 .render()
                 .map_err(|e| error!("pixels.render() failed: {}", e))
                 .is_err()
             {
-                *control_flow = ControlFlow::Exit;
+                event_loop.exit();
                 return;
             }
         }
 
+        let input = &mut app.input;
+        let pixels = &mut app.pixels;
+        let mandelbrot = &mut app.mandelbrot;
         // Handle input events
-        if input.update(event) {
+        if input.process_window_event(&event) {
+
             // Close events
-            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
-                *control_flow = ControlFlow::Exit;
+            if input.key_pressed(KeyCode::Escape) || input.close_requested() {
+                event_loop.exit();
                 return;
             }
 
@@ -146,66 +168,90 @@ fn main() -> Result<(), Error> {
             //    pixels.resize(size.width, size.height);
             //}
             // https://github.com/parasyte/pixels/issues/121
-            pixels.resize(window.inner_size().width, window.inner_size().height);
+            pixels.resize_surface(app.window.inner_size().width, app.window.inner_size().height).unwrap();
 
             // Mandelbrot movement
             {
-            let invert_vertical = true;
-            let up = input.key_pressed(VirtualKeyCode::Up);
-            let down = input.key_pressed(VirtualKeyCode::Down);
-            let left = input.key_pressed(VirtualKeyCode::Left);
-            let right = input.key_pressed(VirtualKeyCode::Right);
-            let zoom_in = input.key_pressed(VirtualKeyCode::X);
-            let zoom_out = input.key_pressed(VirtualKeyCode::Z);
-            let reset = input.key_pressed(VirtualKeyCode::C);
-            let less_iterations = input.key_pressed(VirtualKeyCode::Subtract);
-            let more_iterations = input.key_pressed(VirtualKeyCode::Add) || input.key_pressed(VirtualKeyCode::Equals);
-            
-            let im_range = mandelbrot.im_limits[1] - mandelbrot.im_limits[0];
-            let re_range = mandelbrot.re_limits[1] - mandelbrot.re_limits[0];
-            let shift = 0.1;
-            
-            if up || down || left || right {
-                let mut im_shift = 0.0;
-                let mut re_shift = 0.0;
-                if down { im_shift -= shift };
-                if up { im_shift += shift };
-                if left { re_shift -= shift };
-                if right { re_shift += shift };
-                if invert_vertical {
-                    im_shift *= -1.0;
+                let invert_vertical = true;
+                let up = input.key_pressed(KeyCode::ArrowUp);
+                let down = input.key_pressed(KeyCode::ArrowDown);
+                let left = input.key_pressed(KeyCode::ArrowLeft);
+                let right = input.key_pressed(KeyCode::ArrowRight);
+                let zoom_in = input.key_pressed(KeyCode::KeyX);
+                let zoom_out = input.key_pressed(KeyCode::KeyZ);
+                let reset = input.key_pressed(KeyCode::KeyC);
+                let less_iterations = input.key_pressed(KeyCode::Minus);
+                let more_iterations = input.key_pressed(KeyCode::Equal);
+
+                let im_range = mandelbrot.im_limits[1] - mandelbrot.im_limits[0];
+                let re_range = mandelbrot.re_limits[1] - mandelbrot.re_limits[0];
+                let shift = 0.1;
+
+                if up || down || left || right {
+                    let mut im_shift = 0.0;
+                    let mut re_shift = 0.0;
+                    if down { im_shift -= shift };
+                    if up { im_shift += shift };
+                    if left { re_shift -= shift };
+                    if right { re_shift += shift };
+                    if invert_vertical {
+                        im_shift *= -1.0;
+                    }
+                    mandelbrot.im_limits[0] += im_shift * im_range;
+                    mandelbrot.im_limits[1] += im_shift * im_range;
+                    mandelbrot.re_limits[0] += re_shift * re_range;
+                    mandelbrot.re_limits[1] += re_shift * re_range;
                 }
-                mandelbrot.im_limits[0] += im_shift * im_range;
-                mandelbrot.im_limits[1] += im_shift * im_range;
-                mandelbrot.re_limits[0] += re_shift * re_range;
-                mandelbrot.re_limits[1] += re_shift * re_range;
-            }
-            if zoom_in || zoom_out {
-                let zoom_dir = {
-                    if zoom_in { 1.0 }
-                    else { -1.0 }
-                };
-                mandelbrot.re_limits[0] += zoom_dir * shift * re_range;
-                mandelbrot.re_limits[1] -= zoom_dir * shift * re_range;
-                mandelbrot.im_limits[0] += zoom_dir * shift * im_range;
-                mandelbrot.im_limits[1] -= zoom_dir * shift * im_range;
-            }
-            if reset {
-                mandelbrot = MandelbrotSet::new();
-            }
-            let iterations_delta = 10;
-            if more_iterations {
-                mandelbrot.max_iterations += iterations_delta;
-            }
-            if less_iterations && mandelbrot.max_iterations > iterations_delta {
-                mandelbrot.max_iterations -= 10;
-            }
-            if up || down || left || right || zoom_in || zoom_out || reset || less_iterations || more_iterations {
-                mandelbrot.calculate();
-            }
+                if zoom_in || zoom_out {
+                    let zoom_dir = {
+                        if zoom_in { 1.0 }
+                        else { -1.0 }
+                    };
+                    mandelbrot.re_limits[0] += zoom_dir * shift * re_range;
+                    mandelbrot.re_limits[1] -= zoom_dir * shift * re_range;
+                    mandelbrot.im_limits[0] += zoom_dir * shift * im_range;
+                    mandelbrot.im_limits[1] -= zoom_dir * shift * im_range;
+                }
+                if reset {
+                    std::mem::swap(mandelbrot, &mut MandelbrotSet::new());
+                }
+                let iterations_delta = 10;
+                if more_iterations {
+                    mandelbrot.max_iterations += iterations_delta;
+                }
+                if less_iterations && mandelbrot.max_iterations > iterations_delta {
+                    mandelbrot.max_iterations -= 10;
+                }
+                if up || down || left || right || zoom_in || zoom_out || reset || less_iterations || more_iterations {
+                    mandelbrot.calculate();
+                }
             }
 
-            window.request_redraw();
+            app.window.request_redraw();
         }
-    });
+    }
+
+    fn device_event(&mut self, event_loop: &ActiveEventLoop, device_id: DeviceId, event: DeviceEvent) {
+        let app = self.0.as_mut().unwrap();
+        app.input.process_device_event(&event);
+    }
+
+    fn new_events(&mut self, event_loop: &ActiveEventLoop, cause: StartCause) {
+        if let Some(app) = self.0.as_mut() {
+            app.input.step();
+        }
+    }
+
+    fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
+        let app = self.0.as_mut().unwrap();
+        app.input.end_step();
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+    let event_loop = EventLoop::new()?;
+    let mut app = App(None);
+    event_loop.run_app(&mut app)?;
+    Ok(())
 }
